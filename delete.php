96dd0d26956af9a71420001b211d8b66fe4a83f4
Fitrah Suryani <fitrahsuryani156@gmail.com>
2023-06-05 23:15:21 +0700

    Fix delete.php

--- delete.php
+++ delete.php
@@ -1,29 +1,42 @@
 <?php
 // Mengimpor file konfigurasi
 require_once 'config.php';
+session_start();
 
-// Memeriksa apakah parameter ID telah diberikan
-if (isset($_GET['id'])) {
-    $nib = $_GET['id'];
-
-    // Menjalankan query DELETE untuk menghapus data umkm
-    $query = "DELETE FROM umkm WHERE nib='$nib'";
-    $result = mysqli_query($conn, $query);
-
-    // Memeriksa apakah data berhasil dihapus
-    if ($result) {
-        echo "Data berhasil dihapus.";
-    } else {
-        echo "Terjadi kesalahan: " . mysqli_error($conn);
-    }
+// Cek jika pengguna belum login, alihkan ke halaman login
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: login.php");
+    exit;
 }
 
-// Mengarahkan kembali ke homepage setelah penghapusan
-header("Location: index.php");
-exit();
-?>
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Memeriksa apakah parameter ID telah diterima
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
 
-<?php
-// Menutup koneksi database
-mysqli_close($conn);
+        // Query untuk menghapus data UMKM dari tabel umkm_sleman
+        $querySleman = "DELETE FROM umkm_sleman WHERE nib = ?";
+        $stmtSleman = mysqli_prepare($conn, $querySleman);
+        mysqli_stmt_bind_param($stmtSleman, "s", $id);
+        $resultSleman = mysqli_stmt_execute($stmtSleman);
+
+        // Query untuk menghapus data UMKM dari tabel umkm_bantul
+        $queryBantul = "DELETE FROM umkm_bantul WHERE nib = ?";
+        $stmtBantul = mysqli_prepare($conn, $queryBantul);
+        mysqli_stmt_bind_param($stmtBantul, "s", $id);
+        $resultBantul = mysqli_stmt_execute($stmtBantul);
+
+        if ($resultSleman && $resultBantul) {
+            // Jika penghapusan berhasil, alihkan kembali ke halaman utama
+            header("Location: index.php");
+            exit;
+        } else {
+            echo "Gagal menghapus data.";
+        }
+    } else {
+        echo "Parameter tidak lengkap.";
+    }
+} else {
+    echo "Metode permintaan tidak valid.";
+}
 ?>
